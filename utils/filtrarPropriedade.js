4264bf9e84e2bc72f06bc14b45ad3ecb16a8abec
Damaris Fernandez <helloworld963369@gmail.com>
2025-07-07 21:37:39 -0300

    util

--- utils/filtrarPropriedade.js
+++ utils/filtrarPropriedade.js
@@ -1,29 +1,7 @@
-
-
-// const unidades = JSON.parse(
-//   fs.readFileSync('./data/unidades.json', 'utf-8')
-// );
-
-// export function filterByProperty(property, nameProperty){
-//     // Converte a primeira letra em maiúscula e as seguintes letras da string em minúscula
-//     const capitalizedGenre = nameProperty.charAt(0).toUpperCase() + nameProperty.slice(1).toLowerCase();
-
-//     // Retorna os objetos que contenham a propriedade escolhida e verifica se essa propriedade é um array ou não
-//     return unidades.filter((obj) => {
-//         const prop = obj[property];
-        
-//         if (Array.isArray(prop)) {
-//             return prop.some((p) => p.toLowerCase().startsWith(capitalizedGenre.toLowerCase()));
-//         } else if (typeof prop === 'string') {
-//             return prop.toLowerCase().startsWith(capitalizedGenre.toLowerCase());
-//         }
-//     });
-// } 
-//import fs from 'fs';
 import { readFileSync } from 'fs';
 
 export function filterByProperty(property, value) {
-  const data = readFileSync('./unidades.json', 'utf-8'); // Lê o arquivo JSON
+  const data = readFileSync('./data/unidades.json', 'utf-8'); // Lê o arquivo JSON
   const unidades = JSON.parse(data); // Converte para objeto JS
 
   const capitalized = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
