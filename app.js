77c01a989c181789f83c42b5bdd9b19bc0d856ef
Damaris Fernandez <helloworld963369@gmail.com>
2025-07-09 03:41:57 -0300

    cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 import servicoRoutes from './routes/servicoRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', servicoRoutes);
